8484eb3dbd1aa8533ea6589914f98f5200667e54
Alex Rozgo <alex.rozgo@gmail.com>
2017-05-22 14:10:47 -0600

    auto runs unattended mode

--- LaunchSyntheticsServer.sh
+++ LaunchSyntheticsServer.sh
@@ -4,7 +4,7 @@ SYNTHETICS_DIR=$(pwd)
 PROJECT=$(readlink -f Synthetics/Synthetics.uproject)
 
 cd UnrealEngine/Engine/Binaries/Linux/
-./UE4Editor -project $PROJECT -nullrhi -unattended
+./UE4Editor -project $PROJECT -run -nullrhi -unattended
 cd $SYNTHETICS_DIR
 
 
