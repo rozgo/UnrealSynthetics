5dc1ac814d5ebc53af22431dae9c1e50e861e2a8
Alex Rozgo <alex.rozgo@gmail.com>
2017-05-22 00:16:58 -0600

    paths for ubuntu server

--- GenerateSyntheticsBuildFiles.sh
+++ GenerateSyntheticsBuildFiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SYNTHETICS_DIR=$(pwd)
-PROJECT=$(readlink -f ../Synthetics/Synthetics.uproject)
+PROJECT=$(readlink -f Synthetics/Synthetics.uproject)
 
 cd UnrealEngine
 ./GenerateProjectFiles.sh -project $PROJECT -game -engine
--- LaunchSyntheticsServer.sh
+++ LaunchSyntheticsServer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SYNTHETICS_DIR=$(pwd)
-PROJECT=&(readlink -f ../Synthetics/Synthetics.uproject)
+PROJECT=$(readlink -f Synthetics/Synthetics.uproject)
 
 cd UnrealEngine/Engine/Binaries/Linux/
 ./UE4Editor -project $PROJECT -server -nosteam
