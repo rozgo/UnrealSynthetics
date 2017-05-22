75f7fbaf80cf22517018d59ee81b20c147ccca73
Alex Rozgo <alex.rozgo@gmail.com>
2017-05-22 14:10:34 -0600

    adds full path resolve for ssh script

--- UpdateUnrealEngine.sh
+++ UpdateUnrealEngine.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+SYNTHETICS_DIR=$(pwd)
+
 rsync \
     -avz \
     --exclude-from 'UpdateUnrealEngineExclude.txt' \
-    -e "ssh -F .ssh/Config -p 2222" \
+    -e "ssh -F $SYNTHETICS_DIR/.ssh/Config -p 2222" \
     --progress  \
     unreal@138.68.247.3:/home/unreal/UnrealSynthetics/UnrealEngine/ \
     UnrealEngine/
