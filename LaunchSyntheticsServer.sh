9795452f839489ecf070c7eda990dfe86d6d69f6
Alex Rozgo <alex.rozgo@gmail.com>
2017-05-22 01:11:12 -0600

    configuration that allows to run unattanded on server

--- LaunchSyntheticsServer.sh
+++ LaunchSyntheticsServer.sh
@@ -4,7 +4,7 @@ SYNTHETICS_DIR=$(pwd)
 PROJECT=$(readlink -f Synthetics/Synthetics.uproject)
 
 cd UnrealEngine/Engine/Binaries/Linux/
-./UE4Editor -project $PROJECT -server -nosteam
+./UE4Editor -project $PROJECT -nullrhi -unattended
 cd $SYNTHETICS_DIR
 
 
