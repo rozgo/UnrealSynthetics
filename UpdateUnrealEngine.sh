aad888aba0349b65f17dca546acb7d76c987b3ec
Alex Rozgo <alex.rozgo@gmail.com>
2017-05-22 14:17:17 -0600

    fix case

--- UpdateUnrealEngine.sh
+++ UpdateUnrealEngine.sh
@@ -5,7 +5,7 @@ SYNTHETICS_DIR=$(pwd)
 rsync \
     -avz \
     --exclude-from 'UpdateUnrealEngineExclude.txt' \
-    -e "ssh -F $SYNTHETICS_DIR/.ssh/Config -p 2222" \
+    -e "ssh -F $SYNTHETICS_DIR/.ssh/config -p 2222" \
     --progress  \
     unreal@138.68.247.3:/home/unreal/UnrealSynthetics/UnrealEngine/ \
     UnrealEngine/
