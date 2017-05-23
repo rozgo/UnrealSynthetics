2cbb8e78ca3fb901a4da1604e6d3afc970ae73cf
Alex Rozgo <alex.rozgo@gmail.com>
2017-05-23 02:56:13 -0600

    autogenerate Linux Makefile and optional deps

--- GenerateSyntheticsBuildFiles.sh
+++ GenerateSyntheticsBuildFiles.sh
@@ -3,8 +3,4 @@
 SYNTHETICS_DIR=$(pwd)
 PROJECT=$(readlink -f Synthetics/Synthetics.uproject)
 
-cd UnrealEngine
-./GenerateProjectFiles.sh -project $PROJECT -game -engine
-cd $SYNTHETICS_DIR
-
-
+mono UnrealEngine/Engine/Binaries/DotNET/UnrealBuildTool.exe -makefile -project="$PROJECT" -game -engine
--- LaunchSyntheticsServer.sh
+++ LaunchSyntheticsServer.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
+DEPS=0
+
+if [ $DEPS -eq 1 ]
+then
+
+sudo apt-get update
+sudo apt-get install libpangocairo-1.0-0
+sudo apt-get install libnss3
+sudo apt-get install libgconf-2-4
+sudo apt-get install libxi6
+sudo apt-get install libxcursor1
+sudo apt-get install libxss1
+sudo apt-get install libxcomposite1
+sudo apt-get install libasound2
+sudo apt-get install libxdamage1
+sudo apt-get install libxtst6
+sudo apt-get install libatk1.0-0
+sudo apt-get install libxrandr2
+
+else
+
 SYNTHETICS_DIR=$(pwd)
 PROJECT=$(readlink -f Synthetics/Synthetics.uproject)
 
 cd UnrealEngine/Engine/Binaries/Linux/
-./UE4Editor -project $PROJECT -run -nullrhi -unattended
+./UE4Editor -project $PROJECT -game -nullrhi -unattended
 cd $SYNTHETICS_DIR
+
+fi
+
 
 
 
